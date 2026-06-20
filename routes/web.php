e1be02fcd56d4c413b56929bb8d964f7a7917f19
chiperfox4005 <chiperfox1946@gmail.com>
2026-06-20 19:47:12 +0700

    feat(production): tambahkan route untuk modul produksi (dashboard, detail, update, report)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\ProductionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route Modul Produksi
+Route::prefix('production')->name('production.')->group(function () {
+    Route::get('/', [ProductionController::class, 'index'])->name('index');
+    Route::get('/{id}', [ProductionController::class, 'show'])->name('show');
+    Route::get('/{id}/edit', [ProductionController::class, 'edit'])->name('edit');
+    Route::put('/{id}/update', [ProductionController::class, 'updateProgress'])->name('update');
+    Route::get('/{id}/report', [ProductionController::class, 'generateReport'])->name('report');
 });
